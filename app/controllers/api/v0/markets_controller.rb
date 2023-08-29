1dffe51262c14ddc1e8f74ba6e999be25d4249b6
Gabriel Torres <thomastgtorres21@gmail.com>
2023-08-29 12:31:24 -0500

    update: serializers

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,6 +1,7 @@
 class Api::V0::MarketsController < ApplicationController
   def index
     markets = Market.all
-    render json: MarketSerializer.format_markets(markets)
+    # render json: MarketSerializer.format_markets(markets)
+    render json: MarketSerializer.new(markets)
   end
 end
--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,18 +1,31 @@
 class MarketSerializer
-  def self.format_markets(markets)
-    markets.map do |market|
-      {
-        id: market.id,
-        name: market.name,
-        state: market.state,
-        vendor_count: market.vendors.count,
-        lat: market.lat,
-        lon: market.lon,
-        county: market.county,
-        street: market.street,
-        city: market.city,
-        zip: market.zip
-      }
-    end
+  include JSONAPI::Serializer
+  attributes :name, :state, :vendor_count, :lat, :lon, :county, :street, :city, :zip
+  has_many :vendors
+
+  attribute :vendor_count do |object|
+    object.vendors.count
   end
+  # def self.format_markets(markets)
+  #   markets.map do |market|
+  #     { "data": [
+  #       {
+
+  #         id: market.id,
+  #       # type: market.type,
+  #         attributes: {
+  #           name: market.name,
+  #           state: market.state,
+  #           vendor_count: market.vendors.count,
+  #           lat: market.lat,
+  #           lon: market.lon,
+  #           county: market.county,
+  #           street: market.street,
+  #           city: market.city,
+  #           zip: market.zip
+  #         }
+  #       }
+  #     ] }
+    # end
+  # end
 end
