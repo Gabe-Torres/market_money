5636152f7bd9d26fb78161f674320fa38cf88de9
Gabriel Torres <thomastgtorres21@gmail.com>
2023-08-28 21:04:52 -0500

    refactor models

--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe MarketVendor, type: :model do
   describe 'relationships' do
-    it { should belong_to :market }
-    it { should belong_to :vendor }
+    it { should belong_to(:market) }
+    it { should belong_to(:vendor) }
   end
 end
