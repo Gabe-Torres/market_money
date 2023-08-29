0ac0048e64a45a9959b0edf9d58d4d771a5dfcd7
Gabriel Torres <thomastgtorres21@gmail.com>
2023-08-29 15:00:08 -0500

    refactor: market serializer

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -6,26 +6,4 @@ class MarketSerializer
   attribute :vendor_count do |object|
     object.vendors.count
   end
-  # def self.format_markets(markets)
-  #   markets.map do |market|
-  #     { "data": [
-  #       {
-
-  #         id: market.id,
-  #       # type: market.type,
-  #         attributes: {
-  #           name: market.name,
-  #           state: market.state,
-  #           vendor_count: market.vendors.count,
-  #           lat: market.lat,
-  #           lon: market.lon,
-  #           county: market.county,
-  #           street: market.street,
-  #           city: market.city,
-  #           zip: market.zip
-  #         }
-  #       }
-  #     ] }
-    # end
-  # end
 end
